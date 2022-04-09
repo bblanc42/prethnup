7ad6921dea2984bff5171a495698ae8fb329fe6c
Benoit Blanc <benoitblanc42@gmail.com>
2022-04-09 10:24:15 -0400

    fix: use ethers to get signer

--- frontend/hooks/interact.ts
+++ frontend/hooks/interact.ts
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useCallback, useState } from "react"
 import { useContractFunction } from "@usedapp/core"
-import { utils } from "ethers"
-import { Contract } from "@ethersproject/contracts"
+import { ethers, utils } from "ethers"
 import Prethnup from '../../prethnup-sol/out/Prethnup.sol/Prethnup.json';
 
 export const interact = (toAddr) => {
   const { abi } = Prethnup
+
+  const [provider, setProvider] = useState({});
+  let signer = null;
+
+  useEffect(() => {
+    if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log(`provider: ${provider}`)
+      setProvider(provider);
+      signer = provider.getSigner();
+    }
+    else {
+      console.log(`ethereum is undefined`)
+    }
+  }, []);
+
   const prethnupAddress = "0x101dda533984c0ed196e8643cdc889d4d58a7cd6"
   const prethnupInterface = new utils.Interface(abi)
-  const prethnupContract = new Contract(prethnupAddress, prethnupInterface)
+  const prethnupContract = new ethers.Contract(prethnupAddress, prethnupInterface, signer)
 
-  console.log(prethnupContract)
+  console.log(`signer: ${signer}`)
+  console.log(`contract: ${prethnupContract}`)
 
   // stake
   const { send: stakeSend, state: stakeState } =
